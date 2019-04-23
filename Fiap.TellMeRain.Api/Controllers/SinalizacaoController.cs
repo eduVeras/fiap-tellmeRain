718b46832317cd6a2b27a3ec9c7adc22de165cfc
Eduardo Veras <eduardo.veras@multilaser.com.br>
2019-04-23 11:48:31 -0300

    controladores

---
+++ Fiap.TellMeRain.Api/Controllers/SinalizacaoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiap.TellMeRain.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SinalizacaoController : ControllerBase
+    {
+
+        public void Criar()
+        {
+
+        }
+
+        public void Buscar()
+        {
+
+        }
+
+        public void BuscarPorRegiao()
+        {
+
+        }
+    }
+}
